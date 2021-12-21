11b36984679bb48a6a3608253ba524ae953214fe
jihaneh <jijih@hotmail.fr>
2021-12-21 10:43:09 +0100

    Add Open/Close menu script and Add link to the homepage in the menu

--- assets/js/festival.js
+++ assets/js/festival.js
@@ -1,1 +1,10 @@
 /* Science Festival Site */
+
+// Menu
+document.querySelectorAll('.open_menu img, .close_menu').forEach((element) => {
+  element.addEventListener('click', () => {
+    document.querySelector('.open_menu').classList.toggle('hidden');
+    document.querySelector('.menu').classList.toggle('hidden');
+    document.querySelector('.homepage').classList.toggle('hidden');
+  });
+});
