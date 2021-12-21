cbe679f6174e9667d170ea897032f5934b4960e6
jihaneh <jijih@hotmail.fr>
2021-12-21 13:55:35 +0100

    Add mobile menu effect on scroll

--- assets/js/festival.js
+++ assets/js/festival.js
@@ -8,3 +8,17 @@ document.querySelectorAll('.open_menu img, .close_menu').forEach((element) => {
     document.querySelector('.homepage').classList.toggle('hidden');
   });
 });
+
+const header = document.querySelector('header');
+
+function scrolled() {
+  const currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+  if (currentScroll >= 10) {
+    header.classList.add('header_white');
+  } else {
+    header.classList.remove('header_white');
+  }
+}
+
+document.addEventListener('scroll', scrolled, false);
